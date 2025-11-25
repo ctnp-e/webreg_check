801103a9da3cf9ef734f307d532dbf3112db03c6
mmiivan <catnipiscatweed@gmail.com>
2025-11-24 20:16:47 -0800

    pulls up the display text results

--- test.py
+++ test.py
@@ -18,16 +18,10 @@ driver.get("https://www.reg.uci.edu/perl/WebSoc")
 dept_dropdown = Select(driver.find_element(By.NAME, "Dept")) 
 dept_dropdown.select_by_visible_text("COMPSCI . . . . Computer Science")
 time.sleep(1)
-'''
+
 # 3. Submit search
-search_button = driver.find_element(By.NAME, "Display Text Results")  # example name
-search_button.click()
+driver.find_element(By.XPATH, "//input[@type='submit' and @value='Display Text Results']").click()
 time.sleep(2)  # wait for results to load
 
-# 4. Extract all visible text from the results area
-body_text = driver.find_element(By.TAG_NAME, "body").text
-print(body_text)
 
-# Optionally, parse it (e.g., split by lines, look for “Enr:” etc)
-'''
 driver.quit()
